7d2ff187d9bf94f08a99bb01df4623d1cdb0b5c1
Ranendra Revealize <ryan@revealize.com>
2022-09-01 14:31:48 +0545

    Header

---
+++ front-page.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Jude
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+        <div class="container">
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium iste, quasi perspiciatis expedita quas laudantium blanditiis. Non repellendus ducimus temporibus sapiente. Dolores, fugit illo. Eligendi corporis laborum officia odit eos?</h1>
+        </div>
+	</main><!-- #main -->
+
+<?php
+get_footer();
--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,39 +23,42 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'jude' ); ?></a>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<header>
+			<nav id="site-navigation" class="main-navigation">
+				<div class="d-flex d-md-none justify-content-between align-items-center py-2 px-3">
+					<?php the_custom_logo(); ?>
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"></button>
+				</div>
+				<div class="nav-wrapper">
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+							'menu_class'	 => 'side-menu',
+						)
+					);
+					?>
+					<ul class="nav-social">
+						<li>
+							<a href="#" class="icon-facebook">
+								<?php include get_template_directory() . '/assets/facebook.svg'; ?>
+							</a>
+						</li>
+						<li>
+							<a href="#" class="icon-instagram">
+								<?php include get_template_directory() . '/assets/instagram.svg'; ?>
+							</a>
+						</li>
+						<li>
+							<a href="#" class="icon-paperplane">
+								<?php include get_template_directory() . '/assets/paperplane.svg'; ?>
+							</a>
+						</li>
+					</ul>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$jude_description = get_bloginfo( 'description', 'display' );
-			if ( $jude_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $jude_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'jude' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+				</div>
+			</nav><!-- #site-navigation -->
+		</header>
