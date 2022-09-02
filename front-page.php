458c854137f5c6490dbc6c58caf2691e1611fcbd
Ranendra Revealize <ryan@revealize.com>
2022-09-02 14:03:22 +0545

    Fonts and logo Updates

--- front-page.php
+++ front-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -15,11 +16,118 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-        <div class="container">
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium iste, quasi perspiciatis expedita quas laudantium blanditiis. Non repellendus ducimus temporibus sapiente. Dolores, fugit illo. Eligendi corporis laborum officia odit eos?</h1>
+<main id="primary" class="site-main bg-light">
+    <div class="hero-section">
+        <div class="container-fluid">
+            <div class="row justify-content-lg-end">
+                <div class="col-lg-11 hero-section-wrapper">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="logo-container mb-5">
+                                <?php the_custom_logo(); ?>
+                                <img src="<?php bloginfo('template_directory') ?>/assets/logo-outline.png" alt="Logo Outline" class="logo_outline" />
+                            </div>
+                            <div class="col-lg-10">
+
+                                <h1 class="h3 font-ivy-bold mb-5">
+                                    We support creative artists & give form to vision.
+                                </h1>
+                                <p>We do not take cues, we follow the guidance of inspiration and instinct. In a time of oversharing, JUDE embraces the understated and unspoken. We embrace anomalies and contradictions and believe in what is true, real, and original.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-8 pe-0">
+                            <div class="hero-images-wrapper">
+                                <div class="hero-images">
+                                    <div class="row">
+                                        <div class="col-lg-4 hero-image-col first-col">
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/G L Askew II.jpg" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/Aaron Smith.png" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/Aaron Smith 2.jpg" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/G L Askew II.jpg" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/Aaron Smith.png" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/Aaron Smith 2.jpg" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-4 hero-image-col mid-col">
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/Aaron Smith.png" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/G L Askew II.jpg" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/G L Askew II.jpg" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/Aaron Smith.png" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/Aaron Smith 2.jpg" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/Aaron Smith 2.jpg" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-4 hero-image-col last-col">
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/Aaron Smith 2.jpg" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/G L Askew II.jpg" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/Aaron Smith.png" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/Aaron Smith 2.jpg" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/G L Askew II.jpg" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                            <div class="hero-image">
+                                                <img src="<?php bloginfo('template_directory') ?>/assets/images/Aaron Smith.png" alt="Gallery" class="hero-image" />
+                                                <p class="font-ivy mb-0">G L Askew II</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
-	</main><!-- #main -->
+    </div>
+</main><!-- #main -->
 
 <?php
 get_footer();
