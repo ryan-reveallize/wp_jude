933fe02d11f71cee3a706dda0e8428672e76339a
scottggit <scott@revealize.com>
2022-09-05 20:20:58 +0545

    insta home contact

--- page-contact.php
+++ page-contact.php
@@ -86,17 +86,8 @@ get_header();
 				</div>
 			</div>
 		</div>
+        <?php include('inc/insta-section.php') ?>
 
-		<div class="bottom-section mt-5">
-			<div class="row">
-				<div class="col-12">
-					<h1 class="font-ivy-bold">
-						Follow Us on Instagram
-					</h1>
-					<?php echo do_shortcode('[insta-gallery id="1"]');  ?>
-				</div>
-			</div>
-		</div>
 
 	</div>
 </main><!-- #main -->
