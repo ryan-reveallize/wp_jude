97fcbcf444ab6f08b69053bcbd0cafa03051cdbf
scottggit <scott@revealize.com>
2022-09-05 21:26:04 +0545

    home artist

--- functions.php
+++ functions.php
@@ -239,16 +239,37 @@ function artists_category()
 }
 add_action('init', 'artists_category');
 
-
-function excerpt($num)
-{
-	$limit = $num + 1;
+function excerpt($limit) {
 	$excerpt = explode(' ', get_the_excerpt(), $limit);
-	array_pop($excerpt);
-	$excerpt = implode(" ", $excerpt) . "...";
-	echo $excerpt;
+
+	if (count($excerpt) >= $limit) {
+		array_pop($excerpt);
+		$excerpt = implode(" ", $excerpt) . '...';
+	} else {
+		$excerpt = implode(" ", $excerpt);
+	}
+
+	$excerpt = preg_replace('`\[[^\]]*\]`', '', $excerpt);
+
+	return $excerpt;
 }
 
+function content($limit) {
+  $content = explode(' ', get_the_content(), $limit);
+
+  if (count($content) >= $limit) {
+	  array_pop($content);
+	  $content = implode(" ", $content) . '...';
+  } else {
+	  $content = implode(" ", $content);
+  }
+
+  $content = preg_replace('/\[.+\]/','', $content);
+  $content = apply_filters('the_content', $content); 
+  $content = str_replace(']]>', ']]&gt;', $content);
+
+  return $content;
+}
 // Option page
 if (function_exists('acf_add_options_page')) {
     $parent = acf_add_options_page(array(
